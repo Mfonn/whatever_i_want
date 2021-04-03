cd097fd2cd1784b6d9f075281bf54b0de590787b
Mfon <67243684+Mfonn@users.noreply.github.com>
2021-04-03 07:46:08 +0100

    Add files via upload

---
+++ guessinggame.js
@@ -0,0 +1,148 @@
+var btn = document.getElementById('btn');
+var output = document.getElementById('outputtext');
+
+var number = [Math.floor(Math.random() * 100)]
+
+// for the button to disappear
+ document.getElementById('CrazyClick').style.display = 'none';
+
+btn.addEventListener('click', function(){
+    var input = document.getElementById('userInput').value;
+    if (input == number){
+        output.innerHTML = "You guessed right, click on the button to progress to the next stage"
+        document.getElementById('CrazyClick').style.display = 'block'; 
+    } else if (input < number){
+        output.innerHTML = "You guessed too low!"
+        document.getElementById('CrazyClick').style.display = 'none';
+      
+    };
+       if (input > number) {
+        output.innerHTML = "You guessed too high!"
+        document.getElementById('CrazyClick').style.display = 'none';
+      
+    }  
+
+    
+});
+
+// for the countdown clock
+const FULL_DASH_ARRAY = 283;
+const WARNING_THRESHOLD = 30;
+const ALERT_THRESHOLD = 10;
+
+const COLOR_CODES = {
+  info: {
+    color: "green"
+  },
+  warning: {
+    color: "orange",
+    threshold: WARNING_THRESHOLD
+  },
+  alert: {
+    color: "red",
+    threshold: ALERT_THRESHOLD
+  }
+};
+
+const TIME_LIMIT = 60;
+let timePassed = 0;
+let timeLeft = TIME_LIMIT;
+let timerInterval = null;
+let remainingPathColor = COLOR_CODES.info.color;
+
+document.getElementById("app").innerHTML = `
+<div class="base-timer">
+  <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+    <g class="base-timer__circle">
+      <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+      <path
+        id="base-timer-path-remaining"
+        stroke-dasharray="283"
+        class="base-timer__path-remaining ${remainingPathColor}"
+        d="
+          M 50, 50
+          m -45, 0
+          a 45,45 0 1,0 90,0
+          a 45,45 0 1,0 -90,0
+        "
+      ></path>
+    </g>
+  </svg>
+  <span id="base-timer-label" class="base-timer__label">${formatTime(
+    timeLeft
+  )}</span>
+</div>
+`;
+
+startTimer();
+
+function onTimesUp() {
+  clearInterval(timerInterval);
+}
+
+function startTimer() {
+  timerInterval = setInterval(() => {
+    timePassed = timePassed += 1;
+    timeLeft = TIME_LIMIT - timePassed;
+    document.getElementById("base-timer-label").innerHTML = formatTime(
+      timeLeft
+    );
+    setCircleDasharray();
+    setRemainingPathColor(timeLeft);
+
+    if (timeLeft === 0) {
+      onTimesUp();
+      window.location.reload();
+    }
+  }, 1000);
+}
+
+function formatTime(time) {
+//The largest round integer less than or equal to the result of time divided by 60
+  const minutes = Math.floor(time / 60);
+
+//Seconds are the remainder of the time divided by 60
+  let seconds = time % 60;
+
+//If the value of seconds is less than 10 then put a 0 infront of it
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+ 
+  //The ouput in MM:SS format
+  return `${minutes}:${seconds}`;
+}
+
+function setRemainingPathColor(timeLeft) {
+  const { alert, warning, info } = COLOR_CODES;
+  if (timeLeft <= alert.threshold) {
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.remove(warning.color);
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.add(alert.color);
+  } else if (timeLeft <= warning.threshold) {
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.remove(info.color);
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.add(warning.color);
+  }
+}
+ //Divides the time left by the defined time limit
+function calculateTimeFraction() {
+  const rawTimeFraction = timeLeft / TIME_LIMIT;
+  return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+}
+
+function setCircleDasharray() {
+  const circleDasharray = `${(
+    calculateTimeFraction() * FULL_DASH_ARRAY
+  ).toFixed(0)} 283`;
+  document
+    .getElementById("base-timer-path-remaining")
+    .setAttribute("stroke-dasharray", circleDasharray);
+}
+
